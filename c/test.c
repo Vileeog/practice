1b02ef41a17f6ce903ea903119923491102a8441
vileeog <ikashionyije@gmail.com>
2022-11-27 19:36:40 +0100

    I added another program to the subdirectory

---
+++ c/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  char name[20];
+  printf("What is your name: \n");
+  scanf("%s", name);
+  printf("Your name is %s\n", name);
+  return 0;
+}
